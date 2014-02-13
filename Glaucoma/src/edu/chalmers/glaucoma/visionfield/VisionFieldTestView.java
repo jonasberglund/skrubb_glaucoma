6c00d4d1943ce66315d4e8b071e0d7e3bbed5480
Robin Törnquist <robin@aspire.lan>
2014-02-13 21:13:48 +0100

    refactoring, added Toast that shows the results

--- Glaucoma/src/edu/chalmers/glaucoma/visionfield/VisionFieldTestView.java
+++ Glaucoma/src/edu/chalmers/glaucoma/visionfield/VisionFieldTestView.java
@@ -1,23 +1,16 @@
 package edu.chalmers.glaucoma.visionfield;
 
-import java.util.Observable;
-import java.util.Observer;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
-import android.os.AsyncTask;
-import android.view.MotionEvent;
 import android.view.View;
 
 public class VisionFieldTestView extends View {
 	
 	private Paint paint = new Paint();
 	private PointF dot = null;
-	private DotEngine engine = null;
-	private boolean testIsRunning = false;
 
 	public VisionFieldTestView(Context context) {
 		super(context);
@@ -47,72 +40,13 @@ public class VisionFieldTestView extends View {
 		
 	}
 	
-	private void drawDot(PointF p) {
+	public void drawDot(PointF p) {
 		dot = p;
 		invalidate();
 	}
 	
-	private void removeDot() {
+	public void removeDot() {
 		drawDot(null);
-	}
-	
-	
-	public boolean onTouchEvent(MotionEvent e) {
-		
-		if (e.getAction() == MotionEvent.ACTION_DOWN) {
-			
-			if (testIsRunning) {
-				
-				// Register dot.
-				engine.registerDot();
-				
-			} else {
-				
-				// Initialize the dot engine
-				if (engine == null) {
-					engine = new DotEngine(getWidth(), getHeight());
-				}
-				
-				// Start the test.
-				testIsRunning = true;
-				VisionFieldTestTask testTask = new VisionFieldTestTask();
-				testTask.execute();
-			}
-			
-		}
-		
-		return true;
-	}
-	
-	private class VisionFieldTestTask extends AsyncTask<Integer, Object, Integer> implements Observer {
-
-		@Override
-		protected Integer doInBackground(Integer... params) {
-			
-			engine.addObserver(VisionFieldTestTask.this);
-			engine.runTest();
-			return engine.getResult().size();
-		}
-		
-		@Override
-		protected void onProgressUpdate(Object... progress) {
-			if (progress[0] instanceof PointF) {
-				drawDot((PointF)progress[0]);
-			} else if (progress[0] == null) {
-				removeDot();
-			}
-		}
-		
-		@Override
-		protected void onPostExecute(Integer result) {
-			
-		}
-
-		@Override
-		public void update(Observable observable, Object data) {
-			publishProgress(data);
-		}
-		
 	}
 
 }
